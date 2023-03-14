2e5a61dc56da6af26427d33040e0fda8cad752a8
최동호 <ehdrmfdl002@naver.com>
2023-03-14 23:49:15 +0900

    :art: 1주차 최동호

---
+++ 코테/1주차/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class 폰켓몬 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        answer = Math.min(set.size(), nums.length / 2);
+        return answer;
+    }
+}
