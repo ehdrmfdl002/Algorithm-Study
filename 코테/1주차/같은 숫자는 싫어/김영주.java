bbc704337bf51816445b48c08b1cb05353ab81c7
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-19 15:18:57 +0900

    Update 김영주.java

--- 코테/1주차/같은 숫자는 싫어/김영주.java
+++ 코테/1주차/같은 숫자는 싫어/김영주.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Solution {
+    public int[] solution(int []arr) {
+       ArrayList<Integer> list = new ArrayList<>();
+       list.add(arr[0]);
+       for(int i=1;i<arr.length;i++){
+           if(arr[i]!=arr[i-1]) list.add(arr[i]);
+       }
+       
+       int[] ans = new int[list.size()];
+       for(int i=list.size()-1;i>=0;i--){
+           ans[i] = list.get(i);
+       }
+        
+        return ans;
+    }
+}
