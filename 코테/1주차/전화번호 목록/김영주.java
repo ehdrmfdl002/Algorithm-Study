f6ec261516f4c7e56421767fb50d34d66a668a4e
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-19 13:06:17 +0900

    Update 김영주.java

--- 코테/1주차/전화번호 목록/김영주.java
+++ 코테/1주차/전화번호 목록/김영주.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        
+        for(int i=0;i<phone_book.length;i++){
+            set.add(phone_book[i]);
+        }
+        
+        for(String phone:phone_book){
+            for(int j=1;j<phone.length();j++){
+                if(set.contains(phone.substring(0,j))) return false;
+            }
+        }
+        return true;
+    }
+}
