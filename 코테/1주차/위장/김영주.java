e281ec375f8c9532fb4bc462894283e880f3add5
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-19 13:38:40 +0900

    Update 김영주.java

--- 코테/1주차/위장/김영주.java
+++ 코테/1주차/위장/김영주.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer=1;
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<clothes.length;i++){
+            String cloth_type = clothes[i][1];
+            hm.put(cloth_type,hm.getOrDefault(cloth_type,1)+1);
+        }
+        
+        for(String key: hm.keySet()){
+            answer *= hm.get(key);
+        }
+        
+        return answer-1;
+        
+    }
+}
