09f66ff9431d022233f158bc41f744a73eb15abd
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-25 20:49:35 +0900

    Update 김영주.java

--- 코테/2주차/주식 가격/김영주.java
+++ 코테/2주차/주식 가격/김영주.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+class Solution {
+    public int[] solution(int[] prices) {
+       ArrayList<Integer> list = new ArrayList<>();
+       int start=0;
+       int standard=0;
+       while(true){
+         if(start == prices.length) break;
+         standard = prices[start];
+         int cnt=1;
+         for(int i=start+1;i<prices.length;i++){
+             if(standard>prices[i] || i == prices.length-1){
+                 list.add(cnt);
+                 break;
+             }
+             cnt++;
+          }
+          start++;
+       }
+     list.add(0);
+     return list.stream().mapToInt(i->i).toArray();
+    }
+}
