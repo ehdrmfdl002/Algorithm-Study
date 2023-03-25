e1e96f62eaaa622bfca711491b0c1f9bf9bf9c51
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-25 16:49:39 +0900

    Create 김영주.java

---
+++ 코테/2주차/올바른 괄호/김영주.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+       Stack<Character> stk = new Stack<>();
+       for(int i=0;i<s.length();i++){
+           if(s.charAt(i) == '(') stk.push('(');
+           else if(s.charAt(i) == ')'){
+               if(stk.isEmpty()) return false;
+               stk.pop();
+           }
+       }
+        return stk.isEmpty();
+    }
+}
