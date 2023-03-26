8d03e636320914d56f3ac4d8c60b735e0f26a015
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-03-26 20:52:53 +0900

    Update 김영주.java

--- 코테/2주차/이중우선순위큐/김영주.java
+++ 코테/2주차/이중우선순위큐/김영주.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+class Solution {
+    public int[] solution(String[] operations) {
+       int ans[] = new int[2];
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(String s: operations){
+            String[] arr = s.split(" ");
+            int num = Integer.parseInt(arr[1]);
+            if(arr[0].equals("I")) {
+                maxQ.add(num);
+                minQ.add(num);
+                list.add(num);
+            }else{
+                if(num == -1) maxQ.remove(minQ.poll());
+                else minQ.remove(maxQ.poll());
+            }
+        }
+        ans[0] = maxQ.isEmpty() ? 0: maxQ.peek();
+        ans[1] = minQ.isEmpty() ? 0: minQ.peek();
+        return ans;
+    }
+}
