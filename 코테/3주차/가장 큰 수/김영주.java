da42ac2cccc63c2312309a8a7ba9deb1640792fd
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-04-01 16:34:33 +0900

    Create 김영주.java

---
+++ 코테/3주차/가장 큰 수/김영주.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] str = new String[numbers.length];
+        
+        for(int i=0;i<numbers.length;i++){
+            str[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(str,new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                return (b+a).compareTo(a+b);
+            }
+        });
+        
+        if(str[0].equals("0")) return "0";
+        return String.join("",str);
+    }
+}
