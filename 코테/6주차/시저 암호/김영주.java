b753fc90496a73d8d41a687528575e683e8815b8
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-07 19:56:54 +0900

    Update 김영주.java

--- 코테/6주차/시저 암호/김영주.java
+++ 코테/6주차/시저 암호/김영주.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+class Solution {
+    public String solution(String s, int n) {
+        
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ' ') ans+=" ";
+            else{
+                if(Character.isUpperCase(s.charAt(i))) ans+=(char)((s.charAt(i)-'A'+n)%26+'A');
+                else if(Character.isLowerCase(s.charAt(i))) ans+=(char)((s.charAt(i)-'a'+n)%26+'a');
+            }
+        }
+        return ans;
+    }
+}
