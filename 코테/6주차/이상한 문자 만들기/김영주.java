0f1f870411e87adfc05922f4439655aaec4cf85d
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-07 19:36:56 +0900

    Update 김영주.java

--- 코테/6주차/이상한 문자 만들기/김영주.java
+++ 코테/6주차/이상한 문자 만들기/김영주.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public String solution(String s) {
+        
+        String[] arr = s.split(" ",-1); // 문자열 뒤 공백
+        String ans="";
+        
+        for(int i=0;i<arr.length;i++){
+            String temp = "";
+            for(int j=0;j<arr[i].length();j++){
+                if(j%2==0) temp+=Character.toUpperCase(arr[i].charAt(j));
+                else temp+=Character.toLowerCase(arr[i].charAt(j));
+            }
+            ans+=temp;
+            if(i!=arr.length-1) ans+=" ";
+        }
+        
+        return ans;
+    }
+}
