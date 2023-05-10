79b96ed981a9e6c5175b1e00692da80e664282ca
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-10 22:29:20 +0900

    Update 김영주.java

--- 코테/7주차/GBC/김영주.java
+++ 코테/7주차/GBC/김영주.java
@@ -16,8 +16,8 @@ public class Main
 
         list = new ArrayList<>();
 
-        limit = new int[N][2];
-        test = new int[M][2];
+        limit = new int[N][2]; // 제한 속도
+        test = new int[M][2]; // 실제 운행 속도
 
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
@@ -26,7 +26,7 @@ public class Main
             limit[i][1] = Integer.parseInt(st.nextToken());
 
             if(i>0){
-                limit[i][0]+=limit[i-1][0];
+                limit[i][0]+=limit[i-1][0]; // 제한 속도 구간 구하기
             }
         }
 
@@ -37,7 +37,7 @@ public class Main
             test[i][1] = Integer.parseInt(st.nextToken());
 
             if(i>0){
-                test[i][0]+=test[i-1][0];
+                test[i][0]+=test[i-1][0]; // 실제 운행 속도 구간 구하기
             }
         }
 
@@ -45,11 +45,17 @@ public class Main
             for(int j=0;j<N;j++){
                 if(limit[j][0] == test[i][0]){
                     list.add(test[i][1]-limit[j][1]);
-                    limit[j][1] = 101;
+                    limit[j][1] = 101; 
+                   /* 실제 운행 속도가 제한 속도와 같으면 다음 운행 속도는
+                    다음 제한 속도와 비교한다. 따라서 비교를 하지 못하도록
+                    101을 부여 */
                     }
                 else if(limit[j][0]<test[i][0]){
                     list.add(test[i][1]-limit[j][1]);
                     limit[j][1] = 101;
+                    /* 실제 운행 속도가 제한 속도와 크면 다음 운행 속도는
+                    다음 제한 속도와 비교한다. 따라서 비교를 하지 못하도록
+                    101을 부여 */
                     if(j!=N-1) list.add(test[i][1]-limit[j+1][1]);
                 }
             }
