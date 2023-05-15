a23ba656a4a31de7807a57a9ee5be83782fad731
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-15 19:44:03 +0900

    Update 김영주.java

--- 코테/8주차/문자열 내 p와 y의 개수/김영주.java
+++ 코테/8주차/문자열 내 p와 y의 개수/김영주.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    boolean solution(String s) {
+        int pSum=0;
+        int ySum=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P') pSum++;
+            else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') ySum++;
+        }
+        
+        if((pSum == ySum) || (pSum == 0 && ySum == 0)) return true;
+        else return false;
+    }
+}
