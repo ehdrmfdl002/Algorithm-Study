f346dc9a6df0e2847bff471a9b173aa2c8d77860
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-15 20:48:22 +0900

    Update 김영주.java

--- 코테/8주차/3진법 뒤집기/김영주.java
+++ 코테/8주차/3진법 뒤집기/김영주.java
@@ -1,1 +1,21 @@
+import java.math.*;
 
+class Solution {
+    public int solution(int n) {
+        
+        String s = "";
+        while(n!=0){
+            s+=String.valueOf(n%3);
+            n/=3;
+        }
+        
+        int idx=0;
+        int tot=0;
+        for(int i=s.length()-1;i>=0;i--){
+            tot+=(s.charAt(i)-'0')*Math.pow(3,idx);
+            idx++;
+        }
+        
+        return tot;
+    }
+}
