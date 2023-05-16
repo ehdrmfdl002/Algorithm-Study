db91c020f5ff129d3cb2665bb2c17d871b8f888e
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-16 22:58:42 +0900

    Update 김영주.java

--- 코테/8주차/숫자 문자열과 영단어/김영주.java
+++ 코테/8주차/숫자 문자열과 영단어/김영주.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    static String[] number = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    public int solution(String s) {
+        
+        String answer="";
+        String check="";
+        for(int i=0;i<s.length();i++){
+            if(0<=s.charAt(i)-'a' && s.charAt(i) -'a'< 26){
+                check+=s.charAt(i);
+                if(check.length()>=3){
+                    for(int j=0;j<10;j++){
+                        if(check.contains(number[j])){
+                            answer+=String.valueOf(j);
+                            check="";
+                            break;
+                        }
+                    }
+                }
+            }else answer+=s.charAt(i);
+        }
+        
+        return Integer.valueOf(answer);
+    }
+}
