48e3070af267ba4488b4d3747811bdfcf2d75b57
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-16 21:16:10 +0900

    Update 김영주.java

--- 코테/8주차/이진 변환 반복하기/김영주.java
+++ 코테/8주차/이진 변환 반복하기/김영주.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        
+        while(s.length() > 1){
+            int value = 0;
+            
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i) == '0') answer[1]++;
+                else value++;
+            }
+            
+            s = Integer.toBinaryString(value);
+            answer[0]++;
+        }
+        
+        return answer;
+    }
+}
