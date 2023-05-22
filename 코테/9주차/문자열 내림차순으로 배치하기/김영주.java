4ddab77441695eadb7331b44814b09be5acbcb21
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-22 16:10:11 +0900

    Update 김영주.java

--- 코테/9주차/문자열 내림차순으로 배치하기/김영주.java
+++ 코테/9주차/문자열 내림차순으로 배치하기/김영주.java
@@ -1,1 +1,20 @@
+import java.util.*;
+import java.io.*;
 
+class Solution {
+    public String solution(String s) {
+        
+        ArrayList<Character> list =new ArrayList<>();
+        String answer="";
+        for(int i=0;i<s.length();i++){
+            list.add(s.charAt(i));
+        }
+        
+        Collections.sort(list,Collections.reverseOrder());
+        for(int i=0;i<list.size();i++){
+            answer+=list.get(i);
+        }
+        
+        return answer;
+    }
+}
