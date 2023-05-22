a7730022dbb1f59bca4d4b791d14543c7fbf2ce5
김민식 <kms961101@nate.com>
2023-05-22 16:26:19 +0900

    두 개 뽑아서 더하기

--- 코테/9주차/문자열 내 마음대로 정렬하기/김민식.java
+++ 코테/9주차/문자열 내 마음대로 정렬하기/김민식.java
@@ -1,11 +1,20 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String[] solution(String[] strings, int n) {
+    public static void main(String[] args) {
+        solution(new String[] {"a"}, 2);
+    }
+    public static String[] solution(String[] strings, int n) {
         Arrays.sort(strings, (o1, o2) -> {
             if (o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
             else return o1.charAt(n) - o2.charAt(n);
         });
-        return strings;
+//        return strings;
+        ArrayList<Intger> list = new ArrayList<Intger>();
+        list.add(1);
+        list.add(2);
+        String[] as = list.stream().toArray(new String[list.size()]);
+        return "ee";
     }
 }
