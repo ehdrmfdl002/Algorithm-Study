7ae46f5e46f4374ae6079da175bdaa2f9bb73327
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-05-22 15:57:37 +0900

    Update 김영주.java

--- 코테/9주차/두 개 뽑아서 더하기/김영주.java
+++ 코테/9주차/두 개 뽑아서 더하기/김영주.java
@@ -1,1 +1,16 @@
+import java.util.*;
+import java.io.*;
 
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                hs.add(numbers[i]+numbers[j]);
+            }
+        }
+        
+        return hs.stream().sorted().mapToInt(i->i).toArray();
+    }
+}
