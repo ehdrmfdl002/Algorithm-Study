881825dcb4c5a0d8a2b6566465b45f7d05964b5d
김민식 <kms961101@nate.com>
2023-05-24 13:29:19 +0900

    230524 창고 다각형

--- 코테/9주차/문자열 내 마음대로 정렬하기/김민식.java
+++ 코테/9주차/문자열 내 마음대로 정렬하기/김민식.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    public static void main(String[] args) {
-        solution(new String[] {"a"}, 2);
-    }
     public static String[] solution(String[] strings, int n) {
         Arrays.sort(strings, (o1, o2) -> {
             if (o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
